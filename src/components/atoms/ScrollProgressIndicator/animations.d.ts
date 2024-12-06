338f29821c2b7ef651ff484f4753fa7b2b19ae65
joshuaayoung <joshua@young.net>
2024-12-05 23:10:20 -0700

    try deploying again

--- src/components/atoms/ScrollProgressIndicator/animations.d.ts
+++
@@ -1,5 +0,0 @@
-import { Variants } from 'framer-motion';
-
-export declare const arrowVariants: Variants;
-export declare const pathLengthVariants: Variants;
-export declare const arrowsVariant: Variants;
--- src/components/molecules/HamburgerMenu/animation.d.ts
+++
@@ -1,5 +0,0 @@
-import { Variants } from 'framer-motion';
-
-export declare const menuVariants: Variants;
-export declare const menuItemVariants: Variants;
-export declare const listVariants: Variants;
