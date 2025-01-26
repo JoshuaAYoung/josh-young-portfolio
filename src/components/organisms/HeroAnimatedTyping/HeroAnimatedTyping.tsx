a7e7bf6933ec16a6b6e65eb803c1a80965f969da
unknown <joshua@young.net>
2025-01-25 19:08:13 -0700

    hero typing animation close

--- src/components/organisms/HeroAnimatedTyping/HeroAnimatedTyping.tsx
+++ src/components/organisms/HeroAnimatedTyping/HeroAnimatedTyping.tsx
@@ -1,6 +1,6 @@
 import { forwardRef, useEffect, useRef, useState } from 'react';
 import './HeroAnimatedTyping.scss';
-import { motion, useAnimation } from 'framer-motion';
+import { motion, useAnimation, Variants } from 'framer-motion';
 import heroBackground from '../../../assets/images/hero-background.png';
 import heroPortrait from '../../../assets/images/hero-portrait.png';
 import InViewSection from '../../molecules/InViewSection/InViewSection';
@@ -10,20 +10,12 @@ import SwipeButton from '../../atoms/SwipeButton/SwipeButton';
 import { useScrollToSection } from '../../../utils/useScrollToSection';
 import { useGetAnimations } from './heroAnimations';
 
-const Hero = forwardRef<HTMLElement>((props, ref) => {
+const Hero = forwardRef<HTMLElement>((_props, ref) => {
   // HOOK(S)
   const { scrollToSection } = useScrollToSection();
   const controls = useAnimation();
   const mounted = useRef(false);
-  const {
-    helloVariants,
-    imVariants,
-    firstNameVariants,
-    lastNameVariants,
-    periodVariants,
-    dividerVariants,
-    portraitVariants,
-  } = useGetAnimations();
+  // const { helloVariants, dividerVariants, cursorVariants } = useGetAnimations();
 
   // STATE
   const onSectionInViewActive = useJYStore(
@@ -39,22 +31,16 @@ const Hero = forwardRef<HTMLElement>((props, ref) => {
     mounted.current = true;
 
     const sequence = async () => {
-      await controls.start('initial');
-      await Promise.all([
-        controls.start('dividerIn'),
-        controls.start('helloIn'),
-      ]);
-      controls.start('helloUp');
-      await controls.start('imIn');
-      await controls.start('jyIn');
-      controls.start('lastNameIn');
-      await controls.start('lastNameGrow');
-      await controls.start('lastNameSquish');
-      controls.start('flingPeriod');
-      setTimeout(() => {
-        controls.start('periodBack');
-        setShowSwipeAnimations(true);
-      }, 2000);
+      controls.start('initial');
+      await controls.start('blinkingStart');
+      await controls.start('helloType');
+      await new Promise((resolve) => {
+        setTimeout(resolve, 500);
+      });
+      await controls.start('imType');
+      await controls.start('joshType');
+      await controls.start('youngType');
+      controls.start('blinkingInfinite');
     };
 
     sequence();
@@ -76,6 +62,83 @@ const Hero = forwardRef<HTMLElement>((props, ref) => {
   };
 
   // ANIMATION(S)
+  const containerVarians = {
+    initial: { y: 90 },
+    imType: {
+      y: 0,
+      transition: {
+        duration: 0,
+      },
+    },
+  };
+
+  const getLetterVariants = (eventName: string): Variants => {
+    return {
+      initial: { display: 'none' },
+      [eventName]: {
+        display: 'inline',
+        transition: { duration: 0.1 },
+      },
+    };
+  };
+
+  const getTypeStaggerVariants = (eventName: string): Variants => {
+    return {
+      initial: { display: 'none' },
+      [eventName]: {
+        display: 'inline-block',
+        transition: {
+          staggerChildren: 0.1,
+        },
+      },
+    };
+  };
+
+  const getBreakVariants = (eventName: string): Variants => {
+    return {
+      initial: { display: 'none' },
+      [eventName]: {
+        display: 'block',
+        transition: {
+          duration: 0,
+        },
+      },
+    };
+  };
+
+  const cursorVariants: Variants = {
+    blinkingStart: {
+      opacity: [0, 0, 1, 1],
+      transition: {
+        duration: 1,
+        repeat: 1.5,
+        repeatDelay: 0,
+        ease: 'linear',
+        times: [0, 0.5, 0.5, 1],
+      },
+    },
+    blinkingInfinite: {
+      opacity: [0, 0, 1, 1],
+      transition: {
+        duration: 1,
+        repeat: Infinity,
+        repeatDelay: 0,
+        ease: 'linear',
+        times: [0, 0.5, 0.5, 1],
+      },
+    },
+  };
+
+  const dividerVariants: Variants = {
+    initial: { width: 0 },
+    helloType: {
+      width: 105,
+      transition: {
+        ease: 'linear',
+        duration: 0.5,
+      },
+    },
+  };
 
   return (
     <InViewSection
@@ -97,29 +160,69 @@ const Hero = forwardRef<HTMLElement>((props, ref) => {
         initial="initial"
       >
         <div className="hero-text-container">
-          <div className="hero-clip-path-container">
-            <motion.div className="hero-text-hello" variants={helloVariants}>
-              HELLO!
+          <motion.div
+            className="hero-clip-path-container"
+            variants={containerVarians}
+          >
+            <motion.div
+              className="hero-text-hello"
+              variants={getTypeStaggerVariants('helloType')}
+            >
+              {'HELLO!'.split('').map((letter, index) => (
+                <motion.span
+                  key={index}
+                  variants={getLetterVariants('helloType')}
+                >
+                  {letter}
+                </motion.span>
+              ))}
             </motion.div>
-            <motion.span className="hero-text-im" variants={imVariants}>
-              I'm
-            </motion.span>
-            <motion.span
+            <motion.br variants={getBreakVariants('imType')} />
+            <motion.div
+              className="hero-text-im"
+              variants={getTypeStaggerVariants('imType')}
+            >
+              {"I'm ".split('').map((letter, index) => (
+                <motion.span key={index} variants={getLetterVariants('imType')}>
+                  {letter === ' ' ? '\u00A0' : letter}
+                </motion.span>
+              ))}
+            </motion.div>
+            <motion.div
               className="hero-text-josh"
-              variants={firstNameVariants}
+              variants={getTypeStaggerVariants('joshType')}
             >
-              Josh
-            </motion.span>
-            <motion.span
+              {'Josh'.split('').map((letter, index) => (
+                <motion.span
+                  key={index}
+                  variants={getLetterVariants('joshType')}
+                >
+                  {letter}
+                </motion.span>
+              ))}
+            </motion.div>
+            <motion.br variants={getBreakVariants('youngType')} />
+            <motion.div
               className="hero-text-young"
-              variants={lastNameVariants}
+              variants={getTypeStaggerVariants('youngType')}
             >
-              Young
-              <motion.span className="big-period" variants={periodVariants}>
-                .
-              </motion.span>
-            </motion.span>
-          </div>
+              {'Young.'.split('').map((letter, index) => (
+                <motion.span
+                  key={index}
+                  variants={getLetterVariants('youngType')}
+                  className={
+                    index === 5 ? 'hero-letter big-period' : 'hero-letter'
+                  }
+                >
+                  {letter}
+                </motion.span>
+              ))}
+            </motion.div>
+            <motion.div
+              variants={cursorVariants}
+              className="hero-text-cursor"
+            />
+          </motion.div>
           <motion.div
             className="hero-text-divider"
             variants={dividerVariants}
@@ -139,10 +242,7 @@ const Hero = forwardRef<HTMLElement>((props, ref) => {
             </div>
           )}
         </div>
-        <motion.div
-          className="hero-portrait-container"
-          variants={portraitVariants}
-        >
+        <motion.div className="hero-portrait-container">
           <img
             src={heroPortrait}
             alt="josh young portrait"
--- src/components/organisms/HeroAnimatedTyping/heroAnimations.ts
+++ src/components/organisms/HeroAnimatedTyping/heroAnimations.ts
@@ -1,26 +1,12 @@
 import { Variants } from 'framer-motion';
 
 export const useGetAnimations = () => {
-  const gravityUpDownCurve = [0.3, 0, 0.1, 1];
   const dividerInDuration = 1;
-  const imInDelay = 0.5;
-  const jyInDelay = 0.4;
-
-  const squishDelay = jyInDelay + 0.68;
-  const squishTransition = {
-    type: 'tween',
-    ease: 'easeOut',
-    duration: 0.23,
-    delay: squishDelay,
-  };
-  const squishHeight = 0.04;
-  const flingPeriodDelay = 0.1;
-  const flingPeriodDuration = 0.2;
 
   const helloVariants: Variants = {
     initial: { y: 220, scale: 0.5 },
     // hello pops up and stops
-    helloIn: {
+    helloType: {
       y: [220, 160],
       scale: 1,
       transition: {
@@ -38,230 +24,25 @@ export const useGetAnimations = () => {
         },
       },
     },
-    // hello gets hit by im and pops up high and comes back down with im
-    helloUp: {
-      y: [160, -40, 90],
-      transition: {
-        y: {
-          type: 'tween',
-          ease: gravityUpDownCurve,
-          // lower to make sure we're not going to 110 early with start of lastNameIn
-          duration: 1.88,
-          delay: imInDelay + 0.05,
-        },
-      },
-    },
-    // im hits hello on its way up from young growing
-    lastNameIn: {
-      y: [90, -50, 50],
-      transition: {
-        type: 'tween',
-        ease: gravityUpDownCurve,
-        duration: 0.8,
-        delay: 0.09,
-      },
-    },
-    flingPeriod: {
-      y: 0,
-      transition: {
-        ease: 'easeIn',
-        duration: flingPeriodDuration,
-        delay: flingPeriodDelay,
-      },
-    },
   };
 
-  const imVariants: Variants = {
-    initial: { y: 160 },
-    // im pops up and hits hello
-    imIn: {
-      y: [160, 80],
-      transition: {
-        y: {
-          type: 'tween',
-          ease: 'easeIn',
-          duration: 0.2,
-          delay: imInDelay,
-        },
-      },
-    },
-    // jy hits im and it pops up and comes back down with hello
-    jyIn: {
-      y: [80, -40, 88],
-      transition: {
-        type: 'tween',
-        ease: gravityUpDownCurve,
-        duration: 1.02,
-        delay: jyInDelay + 0.02,
-      },
-    },
-    // as young pops back to full size, flings im up
-    lastNameIn: {
-      y: [88, -30, 40],
-      transition: {
-        type: 'tween',
-        ease: gravityUpDownCurve,
-        duration: 0.8,
-        delay: 0.06,
-      },
-    },
-    flingPeriod: {
-      y: 0,
-      transition: {
-        ease: 'easeIn',
-        duration: flingPeriodDuration,
-        delay: flingPeriodDelay,
-      },
-    },
-  };
-
-  const jyVariants: Variants = {
+  const cursorVariants: Variants = {
     initial: {
-      y: 80,
-      scaleY: 1,
-      filter: 'blur(0px)',
-      backgroundColor: 'var(--primary-color-transparent)',
-    },
-    // jy pops up and hits im and stops, then get squished by im
-    jyIn: {
-      y: [80, 0],
-      scaleY: squishHeight,
-      scaleX: 2.5,
-      backgroundColor: 'var(--primary-color)',
-      filter: 'blur(1px)',
+      opacity: [0, 0, 1, 1],
       transition: {
-        y: {
-          type: 'tween',
-          ease: 'easeOut',
-          duration: 0.2,
-          delay: jyInDelay,
-        },
-        scaleY: squishTransition,
-        scaleX: squishTransition,
-        backgroundColor: {
-          duration: 0.04,
-          delay: squishDelay + 0.18,
-        },
-        filter: {
-          duration: 0.05,
-          delay: squishDelay + 0.14,
-        },
-      },
-    },
-    lastNameIn: {
-      opacity: 0,
-      transition: {
-        duration: 0,
-      },
-    },
-  };
-
-  const firstNameVariants: Variants = {
-    initial: {
-      y: 80,
-      opacity: 0,
-      scaleY: 0.06,
-      scaleX: 0.8,
-    },
-    // josh starts squished then grows to full size as its flung up with im
-    lastNameIn: {
-      y: [80, -30, 40],
-      opacity: 1,
-      scaleY: 1,
-      scaleX: 1,
-      transition: {
-        y: {
-          type: 'tween',
-          ease: gravityUpDownCurve,
-          duration: 0.8,
-          delay: 0.06,
-        },
-        opacity: {
-          duration: 0.1,
-          delay: 0.05,
-        },
-        scaleY: { duration: 0.35, type: 'tween', ease: 'easeIn' },
-        scaleX: { duration: 0.35, type: 'tween', ease: 'easeIn' },
-      },
-    },
-    flingPeriod: {
-      y: 0,
-      transition: {
-        ease: 'easeIn',
-        duration: flingPeriodDuration,
-        delay: flingPeriodDelay,
-      },
-    },
-  };
-
-  const lastNameVariants: Variants = {
-    initial: {
-      opacity: 0,
-      scaleY: squishHeight,
-      backgroundColor: 'var(--primary-color)',
-      filter: 'blur(3px)',
-    },
-    // jy squishes and turns to young and it grows to full size
-    lastNameGrow: {
-      opacity: 1,
-      scaleY: 1,
-      backgroundColor: 'var(--primary-color-transparent)',
-      filter: 'blur(0px)',
-      transition: {
-        backgroundColor: {
-          duration: 0.04,
-          delay: 0.04,
-        },
-        opacity: {
-          duration: 0,
-        },
-        scaleY: { duration: 0.27, type: 'tween', ease: 'easeIn' },
-        filter: {
-          duration: 0.2,
-        },
-      },
-    },
-    // young squishes down as im and josh come back down
-    lastNameSquish: {
-      scaleY: 0.8,
-      scaleX: 1.2,
-      transition: {
-        scaleY: { duration: 0.1, type: 'tween', ease: 'easeOut', delay: 0.33 },
-        scaleX: { duration: 0.1, type: 'tween', ease: 'easeOut', delay: 0.33 },
-      },
-    },
-    flingPeriod: {
-      scaleY: 1,
-      scaleX: 1,
-      transition: {
-        scaleY: { duration: 0.1, type: 'tween', ease: 'easeIn' },
-        scaleX: { duration: 0.1, type: 'tween', ease: 'easeIn' },
-      },
-    },
-  };
-
-  const periodVariants: Variants = {
-    initial: { opacity: 0 },
-    lastNameGrow: { opacity: 1 },
-    flingPeriod: {
-      opacity: 0,
-      transition: {
-        duration: 2,
-        ease: 'easeOut',
-      },
-    },
-    periodBack: {
-      opacity: 1,
-      transition: {
-        duration: 0.2,
+        duration: 1,
+        repeat: Infinity,
+        repeatDelay: 0,
+        ease: 'linear',
+        times: [0, 0.5, 0.5, 1],
       },
     },
   };
 
   const dividerVariants: Variants = {
-    initial: { opacity: 0 },
+    initial: { width: 0 },
     dividerIn: {
-      opacity: 1,
+      width: 100,
       transition: {
         ease: 'easeIn',
         duration: dividerInDuration,
@@ -269,24 +50,9 @@ export const useGetAnimations = () => {
     },
   };
 
-  const portraitVariants: Variants = {
-    initial: { opacity: 0 },
-    flingPeriod: {
-      opacity: 1,
-      transition: {
-        delay: 1.2, // .5 for each circle plus a .2 delay
-      },
-    },
-  };
-
   return {
     helloVariants,
-    imVariants,
-    jyVariants,
-    firstNameVariants,
-    lastNameVariants,
-    periodVariants,
     dividerVariants,
-    portraitVariants,
+    cursorVariants,
   };
 };
